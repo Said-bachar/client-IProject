894ccbecc4938edf6f30a1d0da00f8ba93f5f93e
Bachar Said <bachar@insa-11271>
2021-12-13 13:29:22 +0100

    Adding the part of available games

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('main.html')
+
+@app.route("/choose-game", methods=['POST', 'GET'])
+def choose_game():
+    forward_message = "Moving Forward"
+    # Get game's modes ....
+    # r = requests.get('//API-TO-GAME-SERVER')
+    # json_response = json.dumps(r.json())
+    # rspone = Response(json_response, content_type='application/json; charset=utf-8')
+    # response.headers.add('content-length', len(json_response))
+    # response.status_code = 200
+    # TO-DO : somthings with res ...
+
+    return render_template('menu.html', forward_message=forward_message)
 
 
 
