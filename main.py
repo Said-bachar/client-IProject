46a6cf2aa77c37617f0298a5512b6e0c5b638a86
Said-bachar <saidbachar.uca.ma@gmail.com>
2021-12-30 14:11:26 +0100

    [Said] : adding another GUI and fixing some issues

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify
+import requests
 
 app = Flask(__name__)
 
@@ -16,11 +17,38 @@ def choose_game():
     # response.headers.add('content-length', len(json_response))
     # response.status_code = 200
     # TO-DO : somthings with res ...
+    
 
     return render_template('menu.html', forward_message=forward_message)
 
+@app.route("/add-images", methods=['POST'])
+def add_images():
+    return None
+    # TO-DO
 
 
+@app.route("/new-game", methods=['POST', 'GET'])
+def new_game():
+    return render_template('new-game.html') 
+
+@app.route("/join-game", methods=['POST', 'GET'])
+def join_game():
+    return None
+     # TO-DO
+
+API_URL = "http://192.168.37.69:50000"
+
+@app.route("/test", methods=['GET'])
+def test():
+    res = requests.get(API_URL)
+    return res.json()
+
+@app.route("/some-url", methods=['GET'])
+def get_data():
+    res = requests.get("http://my-api.com")
+    return res.content
+
+     
 
 if __name__ == "__main__":
     app.run(debug = True)   
