81d422e37eecb5c83a2cf5464964cf14f72325fc
Said-bachar <saidbachar.uca.ma@gmail.com>
2022-01-03 09:04:51 +0100

    Update the new game GUI :)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, jsonify
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__) 
 
 @app.route('/')
 def index():
---
+++ static/js/new-g.js
@@ -0,0 +1,68 @@
+var h5 = document.getElementsByTagName('h5')[0];
+var start = document.getElementById('strt');
+var stop = document.getElementById('stp');
+var reset = document.getElementById('rst');
+var sec = 0;
+var min = 0;
+var hrs = 0;
+var t;
+
+function tick(){
+    sec++;
+    if (sec >= 60) {
+        sec = 0;
+        min++;
+        if (min >= 60) {
+            min = 0;
+            hrs++;
+        }
+    }
+}
+function add() {
+    tick();
+    h5.textContent = (hrs > 9 ? hrs : "0" + hrs) 
+            + ":" + (min > 9 ? min : "0" + min)
+            + ":" + (sec > 9 ? sec : "0" + sec);
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+}
+
+timer();
+start.onclick = timer;
+stop.onclick = function() {
+    clearTimeout(t);
+}
+reset.onclick = function() {
+    h5.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
+}
+
+
+// Diaporamaa : --------------------------------------
+
+var diaporama = 1;
+affichage(diaporama);
+
+function boutons(n) {
+affichage(diaporama += n);
+}
+
+function actifIndic(n) {
+affichage(diaporama = n);
+}
+
+function affichage(n) {
+var i;
+var diapoImg = document.getElementsByClassName("diapo");
+
+if (n > diapoImg.length) {diaporama = 1}    
+if (n < 1) {diaporama = diapoImg.length}
+for (i = 0; i < diapoImg.length; i++) {
+    
+    diapoImg[i].style.opacity = "0";
+}
+diapoImg[diaporama-1].style.opacity = "1";    
+
+}
